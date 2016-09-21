bedaaccd68b0f59f17ec33c4188e4b1fa545ca77
sazid <sazidozon@gmail.com>
2016-09-21 13:06:33 +0600

    Add appropriate resources

--- src/com/msazid/game/main/GameMain.java
+++ src/com/msazid/game/main/GameMain.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class GameMain {
 	
-	private static final String GAME_TITLE = "Java Game Development Framework";
+	private static final String GAME_TITLE = "LoneBall";
 	public static final int GAME_WIDTH = 800;
 	public static final int GAME_HEIGHT = 450;
 	public static Game sGame;
